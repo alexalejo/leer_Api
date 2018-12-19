2aa1996084258989ad0f1c6a2583a3fd77fe6665
Alex Alejo <alexalejo0@hotmail.com>
2018-12-19 16:29:57 -0300

    Primer Commit

---
+++ Leer_Api/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using Newtonsoft.Json;
+using System.Runtime.Remoting.Messaging;
+using System.IO;
+
+namespace Leer_Api
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            string url = "https://api.cambio.today/v1/full/USD/json?key=1524|fp01KD82DW3~_PAnGj2a0EqG~QJ1qq4Z";
+            var json = new WebClient().DownloadString(url);
+            dynamic m = JsonConvert.DeserializeObject(json);
+            
+            foreach (var i in m)
+            {
+                
+                Console.WriteLine(i);
+            }
+            Console.Read();
+        }
+    }
+}
